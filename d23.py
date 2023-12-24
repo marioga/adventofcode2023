02512d57d89f295002cecd2838786d89db6e9123
Mario Garcia <marioga.development@gmail.com>
2023-12-23 21:13:48 -0800

    Coded up better solution for part 2 of Day 23 after reading hints

--- d23.py
+++ d23.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 from pathlib import Path
 
 DIRS = {
@@ -10,7 +11,7 @@ DIRS = {
 }
 
 
-def get_paths(mat, m, n, start, end, longest_path, curr=None, visited=None, part1=True):
+def get_longest_path(mat, m, n, start, end, longest_path, curr=None, visited=None):
     if curr is None:
         curr = [start]
         visited = {start}
@@ -22,16 +23,68 @@ def get_paths(mat, m, n, start, end, longest_path, curr=None, visited=None, part
         return
 
     r, c = curr[-1]
-    _dirs = DIRS[mat[r][c]] if part1 else DIRS['.']
-    for dr, dc in _dirs:
+    for dr, dc in DIRS[mat[r][c]]:
         nr, nc = r + dr, c + dc
         if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] != '#' and (nr, nc) not in visited:
             visited.add((nr, nc))
             curr.append((nr, nc))
-            get_paths(mat, m, n, start, end, longest_path, curr, visited, part1)
+            get_longest_path(mat, m, n, start, end, longest_path, curr, visited)
             # backtrack
             visited.remove((nr, nc))
             curr.pop()
+
+
+def get_condensed_graph(mat, m, n, start, end):
+    graph = {start: {}, end: {}}
+    for r in range(m):
+        for c in range(n):
+            if mat[r][c] == '#':
+                continue
+
+            neigh = 0
+            for dr, dc in DIRS['.']:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] != '#':
+                    neigh += 1
+
+            if neigh >= 3:
+                graph[(r, c)] = {}
+
+    vertices = set(graph.keys())
+    for r, c in vertices:
+        seen = {(r, c)}
+        queue = deque([(0, r, c)])
+        while queue:
+            d, cr, cc = queue.popleft()
+            for dr, dc in DIRS['.']:
+                nr, nc = cr + dr, cc + dc
+                if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] != '#' and (nr, nc) not in seen:
+                    if (nr, nc) in vertices:
+                        graph[(r, c)][(nr, nc)] = d + 1
+                    else:
+                        queue.append((d + 1, nr, nc))
+                    seen.add((nr, nc))
+
+    return graph
+
+
+def get_longest_path_in_graph(vertex, graph, end, longest_path, dist=0, visited=None):
+    if visited is None:
+        visited = {start}
+
+    if vertex == end:
+        if not longest_path:
+            longest_path.append(dist)
+        elif longest_path[0] < dist:
+            longest_path[0] = dist
+        return
+
+    for next_vertex, weight in graph[vertex].items():
+        if next_vertex not in visited:
+            visited.add(next_vertex)
+            get_longest_path_in_graph(next_vertex, graph, end, longest_path, dist + weight, visited)
+            # backtrack
+            visited.remove(next_vertex)
 
 
 if __name__ == '__main__':
@@ -54,10 +107,12 @@ if __name__ == '__main__':
 
     # Part 1
     longest_path = []
-    get_paths(mat, m, n, start, end, longest_path)
+    get_longest_path(mat, m, n, start, end, longest_path)
     print(longest_path[0])
 
-    # Part 2... this is slow. Rethink at some point
+    # Part 2
+    graph = get_condensed_graph(mat, m, n, start, end)
     longest_path = []
-    get_paths(mat, m, n, start, end, longest_path, part1=False)
+    get_longest_path_in_graph(start, graph, end, longest_path)
     print(longest_path[0])
+
